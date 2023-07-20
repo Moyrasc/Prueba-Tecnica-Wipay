95e2228bdefdad2e16d9e8cddcf731fc841d2fad
Moyrasc <noemisolac@gmail.com>
2023-07-20 18:38:09 +0200

    complete view home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,56 @@
-import { Container, ContainerParagraph, ContainerTitle, Title } from './HomeStyled'
+import { Container, ContainerTitle, ContentImg, ContentSection, Section, Title } from './HomeStyled'
+import calidad from '../assets/calidad.png'
+import esfuerzo from '../assets/esfuerzo.png'
+import liderazgo from '../assets/liderazgo.png'
+import precision from '../assets/precision.png'
+import rapidez from '../assets/rapidez.png'
+import seguridad from '../assets/seguridad.png'
 
 const Home = () => {
   return (
     <Container>
       <ContainerTitle>
         <Title>Bienvenido a <span>Wipay</span></Title>
-        <ContainerParagraph>
-          <p>Empresa innovadora tanto en soluciones tecnológicas como en procesos productivos de trabajo. El entendimiento de procesos complejos y la adaptación al usuario hacen que Wipay crezca día a día</p>
-
-        </ContainerParagraph>
-
       </ContainerTitle>
+      <div>
+
+        <ContentImg>
+          <ContentSection>
+            <Section>
+              <img src={liderazgo} alt='icon' style={{ width: '80' + 'px' }} />
+              <h3>Liderazgo</h3>
+              <p>Creemos en nuestro proyecto, sacando lo mejor de cada uno de nosostros.</p>
+            </Section>
+            <Section>
+              <img src={rapidez} alt='rocket' style={{ width: '80' + 'px' }} />
+              <h3>Rapidez</h3>
+              <p>Mantenemos una velocidad constante y fluida para lograr los objetivos propuestos.</p>
+            </Section>
+            <Section>
+              <img src={precision} alt='target' style={{ width: '80' + 'px' }} />
+              <h3>Precision </h3>
+              <p>Organizamos cada uno de los servicios para dar respuesta a cualquier problema.</p>
+            </Section>
+          </ContentSection>
+          <ContentSection>
+            <Section>
+              <img src={seguridad} alt='shield' style={{ width: '80' + 'px' }} />
+              <h3>Seguridad </h3>
+              <p>Protección en todas las vías de negocio para reducir riesgos en cada cliente.</p>
+            </Section>
+            <Section>
+              <img src={calidad} alt='quality' style={{ width: '80' + 'px' }} />
+              <h3>Calidad </h3>
+              <p>Nuestra misión diaria es tener presente las expectativas de nuestros clientes.</p>
+            </Section>
+            <Section>
+              <img src={esfuerzo} alt='effort' style={{ width: '80' + 'px' }} />
+              <h3>Esfuerzo </h3>
+              <p>Trabajamos para lograr nuevos retos como la expansión empresarial de negocio.</p>
+            </Section>
+          </ContentSection>
+        </ContentImg>
+      </div>
 
     </Container>
   )
--- src/views/HomeStyled.jsx
+++ src/views/HomeStyled.jsx
@@ -4,37 +4,64 @@ const Container = styled.div`
 align-items: center;
 display: flex;
 height: 80vh;
-justify-content: center;
 width: 100%;
+flex-direction: column;
+justify-content: space-between;
 `
 const ContainerTitle = styled.div`
 display:flex;
 justify-content: space-around;
 flex-direction:column;
 align-items: center;
+padding: 2.5rem 0rem;
 `
 const Title = styled.h1`
 font-size: 3rem;
 font-weight:bold;
+    text-transform: uppercase;
+    font-family: 'Poppins', sans-serif;
 & span {
     color: red;
 }
 `
-const ContainerParagraph = styled.div`
-padding:1rem 0rem;
-margin: 20px;
-display: flex;
-flex-direction: column;
-& p {
-    font-size: 18px;
-    text-align:center;
-}
-`
 
+const ContentValues = styled.div`
+`
+const ContentImg = styled.div`
+display: flex;
+width: 900px;
+height:600px;
+    border: 1px solid #0c2846;
+    border-radius: 10px;
+    padding: 1rem;
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.18);
+-moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.18);
+box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.18);
+`
+const ContentSection = styled.section`
+`
+const Section = styled.section`
+display: grid;
+justify-items: center;
+text-align: center;
+width: 95%;
+& p {
+  margin: 1rem 1rem;
+}
+& h3 {
+  color: #0c2846;
+  font-size: 26px
+}
+
+`
 export {
   Container,
   ContainerTitle,
   Title,
-  ContainerParagraph
+
+  ContentValues,
+  ContentImg,
+  ContentSection,
+  Section
 
 }
